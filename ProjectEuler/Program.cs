b25ccc6e80d8c79889367d0f83b2126a21a48c87
Rahul Panday <rpanday@knoema.com>
2016-01-05 15:30:09 +0530

    Added RunAllPrograms method

--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -7,11 +7,17 @@ namespace ProjectEuler
 	{
 		static void Main(string[] args)
 		{
+			//Problem1.Run(); //to run individual Problem
+			RunAllPrograms();
+			Console.WriteLine("Press any key to close");
+			Console.ReadKey();
+		}
+
+		static void RunAllPrograms()
+		{
 			Problem1.Run();
 			Problem2.Run();
 
-			Console.WriteLine("Press any key to close");
-			Console.ReadKey();
 		}
 	}
 }
